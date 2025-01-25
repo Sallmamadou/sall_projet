e91b7b9e5afeea37248ee09b6b9349e3ac2e15e7
sallmamadou <sallmemedou2018@gmail.com>
2025-01-25 21:49:45 +0100

    commit

---
+++ src/main/java/projet_springboot/java/sall_projet/SallProjetApplication.java
@@ -0,0 +1,13 @@
+package projet_springboot.java.sall_projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SallProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SallProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projet_springboot/java/sall_projet/SallProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package projet_springboot.java.sall_projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SallProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
